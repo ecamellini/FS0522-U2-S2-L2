a283805bf52fb60d65cbe7562e98b6c69394ae1f
Eric Camellini <eric.camellini@gmail.com>
2022-10-04 13:26:04 +0200

    Adding search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,30 +10,41 @@ let productsList = [
 // Ma comunque avrò sempre in mano una lista di oggetti!
 
 function visualizeProducts() {
-  // 1) un ciclo, cioè, per ogni elemento della lista...
+  // Per prima cosa, svuoto la div che contiene i prodotti.
+  // Così, tutte le volte che chiamo questa funzione, cioè, a ogni
+  // ricerca dell'utente, visualizzerò solo i prodotti che corrispondono alla ricerca.
+  document.getElementById('product-list').innerHTML = ''
 
+  // 1) Per ogni elemento della lista dei prodotti...
   for (let index = 0; index < productsList.length; index++) {
     const product = productsList[index];
 
-    // 2) creo una div, avrà classe card e le varie div al suo interno
-    let productDiv = `
-    <div class="card product-card">
-      <img
-        src="${product.image}"
-        class="card-img-top"
-        alt="${product.name}"
-      >
-      <div class="card-body">
-        <h5 class="card-title">${product.name}</h5>
-        <p class="card-text">${product.price}</p>
-        <a href="#" class="btn btn-primary"><i class="bi bi-cart-plus-fill"></i></a>
-      </div>
-    </div>`
+    // Prendo quello che ha scritto l'utente nella ricerca
+    let searchQuery = document.getElementById("product-search").value
 
-    document.getElementById('product-list').innerHTML += productDiv
+    if (product.name.includes(searchQuery)) { // Se è contenuto nel nome del prodotto
+
+      // Allora aggiungo una div per quel prodotto
+      let productDiv = `
+      <div class="card product-card">
+        <img
+          src="${product.image}"
+          class="card-img-top"
+          alt="${product.name}"
+        >
+        <div class="card-body">
+          <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.price}</p>
+          <a href="#" class="btn btn-primary"><i class="bi bi-cart-plus-fill"></i></a>
+        </div>
+      </div>`
+
+      document.getElementById('product-list').innerHTML += productDiv
+    }
 
     // SE AVESSIMO VOLUTO USARE LE UTILITIES DI DOCUMENT PER CREARE I TAG...
-    // Però vediamo che è un po' verboso come approccio.
+    // Vedi il codice seguente...
+    // È un po' verboso come approccio.
     // Questo è quello che Angular e/o React e/o Vue vi aiuteranno a fare.
 
     // Creo la card
